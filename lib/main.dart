ee0e025c0963bd557bc98a6f59829cd55a7d5d28
MathsPrado <matheusprado55@gmail.com>
2020-05-05 20:21:18 -0300

    First Hello World

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyCoolApp());
+}
+class MyCoolApp extends StatelessWidget {
+ Widget build(BuildContext context){
+   return MaterialApp(home: Text('Hello World!'),);
+  }
+}
