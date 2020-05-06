f0199a308686ed96a18ed658888bd9588510b57f
MathsPrado <matheusprado55@gmail.com>
2020-05-05 22:47:05 -0300

    Adding Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MyCoolApp());
 class MyCoolApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite Pokemon?',
+      'What\s your pokemon game favorite?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is My Defoult Text!'),
+        body: Column(
+          children: [
+            Text('The Question!'),
+            RaisedButton( child: Text('Answer 1'), onPressed: null,),
+            RaisedButton( child: Text('Answer 2'), onPressed: null),
+              RaisedButton( child: Text('Answer 3'), onPressed: null),
+          ],
+        ),
       ),
     );
   }
